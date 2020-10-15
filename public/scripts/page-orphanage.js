50d7349735cf5e39c64ca770923a4f42659c650b
Jefferson Fantasia <jefferson.fantasia@gmail.com>
2020-10-14 23:26:24 -0300

    day03

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,49 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
+
+//create map
+const map = L.map("mapid", options).setView([-27.222633, -49.6455874], 15);
+
+//create and add tile layer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+//create icon layer
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchir: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+//create and add marker
+L.marker([-27.222633, -49.6455874], { icon }).addTo(map);
+
+/*image galery */
+
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  //remover todas as classes .active
+  const buttons = document.querySelectorAll(".images button");
+
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
+
+  buttons.forEach(removeActiveClass);
+
+  //selecionar imagem clicada
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+
+  //atualizar container de imagem
+  imageContainer.src = image.src;
+
+  //adicionar a classe .active para o botão clicado
+  button.classList.add("active");
+}
